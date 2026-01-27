b33eaabd2b2cba40293c42a7122502ded905b20a
MacBook Pro <macbookpro@Sturan.local>
2026-01-27 14:32:23 +0300

    fix(android): include gradle-cli for wrapper

--- mouse-scheduler-android/gradlew.bat
+++ mouse-scheduler-android/gradlew.bat
@@ -4,6 +4,7 @@ setlocal
 set APP_HOME=%~dp0
 set WRAPPER_JAR=%APP_HOME%gradle\wrapper\gradle-wrapper.jar
 set WRAPPER_SHARED_JAR=%APP_HOME%gradle\wrapper\gradle-wrapper-shared.jar
+set GRADLE_CLI_JAR=%APP_HOME%gradle\wrapper\gradle-cli.jar
 
 if defined JAVA_HOME (
   set JAVA_CMD=%JAVA_HOME%\bin\java.exe
@@ -11,7 +12,7 @@ if defined JAVA_HOME (
   set JAVA_CMD=java
 )
 
-set CLASSPATH=%WRAPPER_JAR%;%WRAPPER_SHARED_JAR%
+set CLASSPATH=%WRAPPER_JAR%;%WRAPPER_SHARED_JAR%;%GRADLE_CLI_JAR%
 
 "%JAVA_CMD%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
 endlocal
