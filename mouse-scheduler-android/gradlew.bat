0a9ff554b59cf7b12ed8f29d40bd36ee2dca5274
MacBook Pro <macbookpro@Sturan.local>
2026-01-27 14:23:15 +0300

    chore(android): add Gradle wrapper

---
+++ mouse-scheduler-android/gradlew.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal
+
+set APP_HOME=%~dp0
+set WRAPPER_JAR=%APP_HOME%gradle\wrapper\gradle-wrapper.jar
+set WRAPPER_SHARED_JAR=%APP_HOME%gradle\wrapper\gradle-wrapper-shared.jar
+
+if defined JAVA_HOME (
+  set JAVA_CMD=%JAVA_HOME%\bin\java.exe
+) else (
+  set JAVA_CMD=java
+)
+
+set CLASSPATH=%WRAPPER_JAR%;%WRAPPER_SHARED_JAR%
+
+"%JAVA_CMD%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+endlocal
+
